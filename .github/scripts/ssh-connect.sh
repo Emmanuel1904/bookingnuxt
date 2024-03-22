59cd6db1ce5fda11afb8db60e91e1fe188d5cfa7
Emmanuel MEMEL <ememel@omnilog.fr>
2024-03-23 00:38:20 +0100

    update sh

--- .github/scripts/ssh-connect.sh
+++ .github/scripts/ssh-connect.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 repro_git="https://github.com/Emmanuel1904/bookingnuxt.git"
+wk_dir="bookingprod"
 
 # Vérifie si les variables d'environnement nécessaires sont définies
-if [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$WORK_DIR" ] || [ -z "$MAIN_BRANCH" ]; then
+if [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$wk_dir" ] || [ -z "$MAIN_BRANCH" ]; then
     echo "Certaines variables d'environnement ne sont pas définies."
     exit 1
 fi
 
 # check if current folder if empty: "$(ls -A .)" before git pull || git clone form origin
-ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && git checkout $MAIN_BRANCH && 
+ssh $SSH_USER@$SSH_HOST "cd $wk_dir && git checkout $MAIN_BRANCH && 
 if [ \"\$(ls -A .)\" ]; then 
     git pull --strategy-option theirs
 else
@@ -17,7 +18,7 @@ else
     git clone $repro_git .
 fi &&
 docker run -dp 3007:3000 \
-    -w \"/$WORK_DIR\" --net=nginx_network --name dockerbookingcont_ --mount type=bind,src=\"./\",target=/$WORK_DIR \
+    -w \"/$wk_dir\" --net=nginx_network --name dockerbookingcont_ --mount type=bind,src=\"./\",target=/$wk_dir \
     node:18-alpine \
     sh -c \"npm install && npm run dev\" &&
 exit"
