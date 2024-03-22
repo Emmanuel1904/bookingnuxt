e8ce496e0fc8fc2b6a242b384f488779e5cc1da6
Emmanuel MEMEL <ememel@omnilog.fr>
2024-03-23 00:34:57 +0100

    update sh

--- .github/scripts/ssh-connect.sh
+++ .github/scripts/ssh-connect.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-repro_git="https://github.com/Emmanuel1904/bookingnuxt.git"
-
-# Vérifie si les variables d'environnement nécessaires sont définies
-if [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$WORK_DIR" ] || [ -z "$MAIN_BRANCH" ]; then
-    echo "Certaines variables d'environnement ne sont pas définies."
-    exit 1
-fi
-
-# check if current folder if empty: "$(ls -A .)" before git pull || git clone form origin
-ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && git checkout $MAIN_BRANCH && 
-if [ \"\$(ls -A .)\" ]; then 
-    git pull --strategy-option theirs
-else
-    git remote add origin $repro_git &&
-    git clone $repro_git .
-fi &&
-exit"
+ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && git checkout $MAIN_BRANCH && git pull && exit"
 
 rm -rf ~/.ssh
-# docker compose up -d --remove-orphans
