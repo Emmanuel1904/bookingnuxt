f91e124b0c167ce327d5e473c164180da3065ff6
Emmanuel MEMEL <35105260+Emmanuel1904@users.noreply.github.com>
2024-03-24 22:28:03 +0100

    Feat/book 02 setup docker infra deploy (#51)
    * update flow
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update sh
    
    * update DOCKER path
    
    * update DOCKER run script
    
    * update DOCKER run script
    
    * initial commit
    
    ---------
    
    Co-authored-by: Emmanuel MEMEL <ememel@omnilog.fr>

--- .github/scripts/ssh-connect.sh
+++ .github/scripts/ssh-connect.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && git checkout $MAIN_BRANCH && git pull && exit"
+repro_git=https://github.com/Emmanuel1904/bookingnuxt.git
+
+# Vérifie si les variables d'environnement nécessaires sont définies
+if [ -z "$SSH_USER" ] || [ -z "$SSH_HOST" ] || [ -z "$WORK_DIR" ] || [ -z "$MAIN_BRANCH" ]; then
+    echo "Certaines variables d'environnement ne sont pas définies."
+    exit 1
+fi
+
+# check if current folder if empty: "$(ls -A .)" before git pull || git clone form origin
+ssh $SSH_USER@$SSH_HOST "cd $WORK_DIR && 
+if [ \"\$(ls -A .)\" ]; then
+    git checkout $DEV_BRANCH && git pull --strategy-option theirs
+else
+    git clone $repro_git . && git checkout $DEV_BRANCH
+fi &&
+docker rm -fv dockerbookingprod &&
+docker run -dp 3007:3000 \
+    -w \"/bookingprod\" --net=nginx_network --name dockerbookingprod --mount type=bind,src=/root/workspace/bookingprod,target=/bookingprod \
+    node:18-alpine \
+    sh -c \"cd /bookingprod cp -r . . && npm install && npm run dev\" &&
+exit"
 
 rm -rf ~/.ssh
